9651d72ab9fc440e6f50a99a5e959c5aa07a09b3
Linus Callheim <stoltefar@gmail.com>
2021-10-28 11:33:22 +0200

    Fixat slumpmetod för Stor bokstav och test av den i mainmetoden

--- SafeWord/SafeWord.java
+++ SafeWord/SafeWord.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class SafeWord {
   public static void main(String[] args) {
-    
+    boolean missingCapital = true;
+    boolean missingCommon = true;
+    boolean missingDigit = true;
+    boolean missingSpecial = true;
+
+    String safeWord ="";
+    for (int i=0; i< 20; ++i) {
+      safeWord = addCapitalChar(safeWord);
+      System.out.println(safeWord);
+    }
+
+  }
+
+  static String addCapitalChar(String password) {
+    Random r = new Random();
+    int intValue = r.nextInt(26) + 65;
+    char capital = (char)intValue;
+    password = password.concat(String.valueOf(capital));
+    return password;
   }
 }
